1d043cc9df4c17e06eaa03a0ad322846bb4a6027
hassan-ish <hassan.abuishaiba@gmail.com>
2022-12-20 15:38:50 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <Counter />
-      <Counter onIncrement="2" onDecrement="2" />
-      <Counter onIncrement="5" onDecrement="10" />
+      <Counter steps="2" />
+      <Counter steps="5" />
     </div>
   );
 }
--- src/Counter/index.jsx
+++ src/Counter/index.jsx
@@ -4,29 +4,26 @@ export default class Counter extends Component {
   state = {
     counter: 0,
   };
+  n = this.props.steps;
   onIncrement = () => {
-    let n = this.props.onIncrement;
-    n = n > 0 ? Number(n) : 1;
+    this.n = this.n > 0 ? parseInt(this.n) : 1;
     console.log();
     this.setState((prevState) => ({
-      counter: prevState.counter + n,
+      counter: prevState.counter + this.n,
     }));
   };
   onDecrement = () => {
-    let n = this.props.onDecrement;
-    n = n > 0 ? Number(n) : 1;
+    this.n = this.n > 0 ? parseInt(this.n) : 1;
     if (this.state.counter > 0) {
       this.setState((prevState) => ({
-        counter: prevState.counter - n,
+        counter: prevState.counter - this.n,
       }));
     }
   };
   render() {
     return (
       <div>
-        <span className="num">
-          {this.state.counter > 0 ? this.state.counter : 0}
-        </span>
+        <span>{this.state.counter}</span>
         <button onClick={this.onIncrement}>+</button>
         <button onClick={this.onDecrement}>-</button>
       </div>
